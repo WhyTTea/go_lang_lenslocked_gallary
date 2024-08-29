4c71420f46b202ee78f5621bb92760f3e51b4623
Yuriy Teodorovych <Yuriy@ibm.com>
2024-08-29 16:43:08 -0400

    Initial commit. Connecting local to remotre

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/whyttea/lenslocked
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>All good in da hood and air is working</h1>")
+
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>FAQ page</h1><p>Q: How do I contact support? <p>A: Email us - <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+}
+
+type Router struct {
+}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		// TODO: add the defualt page
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		// TODO: add the defualt page
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
+func main() {
+	var router Router
+	// http.HandleFunc("/contact", contactHandler)
+	fmt.Println("Starting the server at :3000... ")
+	http.ListenAndServe("localhost:3000", router)
+}
