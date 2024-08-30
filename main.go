8d24f8c8c686041c51a3c98025aa814e062bd8fe
Yuriy Teodorovych <Yuriy@ibm.com>
2024-08-29 22:40:29 -0400

    Added chi router

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,22 +23,22 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>FAQ page</h1><p>Q: How do I contact support? <p>A: Email us - <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
-type Router struct {
-}
+// type Router struct {
+// }
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		// TODO: add the defualt page
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	case "/faq":
+// 		faqHandler(w, r)
+// 	default:
+// 		// TODO: add the defualt page
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
 
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
@@ -51,8 +53,14 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // }
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server at :3000... ")
-	http.ListenAndServe("localhost:3000", router)
+	http.ListenAndServe("localhost:3000", r)
 }
