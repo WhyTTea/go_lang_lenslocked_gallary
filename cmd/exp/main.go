27daf06c45cc5ac20af7683e18fbc9a2705addf3
Yuriy Teodorovych <Yuriy@ibm.com>
2024-09-04 20:39:11 -0400

    Added views, separated code to follow MVC methodology

---
+++ cmd/exp/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Bio  string
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	user := User{
+		Name: "John Smith",
+		Bio:  
+	}
+
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
--- main.go
+++ main.go
@@ -2,25 +2,51 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Println("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template file.", http.StatusInternalServerError)
+		return
+	}
+	viewTpl := views.Template{
+		htmlTpl: tpl,
+	}
+	viewTpl.Execute(w, data)
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>All good in da hood and air is working</h1>")
+
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+
+	// IDparam := chi.URLParam(r, "id")
+	// ctx := r.Context()
+	// key := ctx.Value("key").(string)
+
+	// w.Write([]byte(fmt.Sprintf("New ID is %v, %v", IDparam, key)))
+	// response := fmt.Sprintf("<h2>%s</h2>", IDparam)
+	// fmt.Fprint(w, response)
 
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Contact page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.")
+
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>FAQ page</h1><p>Q: How do I contact support? <p>A: Email us - <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	// alternative to pathing the var with just joining inside of the func call
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 // type Router struct {
